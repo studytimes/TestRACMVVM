cc363c159b0b5ae656862eefb8675fc7201e2b55
JiangShuai <825663508@qq.com>
2017-02-28 10:19:28 +0800

    测试信息

---
+++ TestRACMVVM/TestRACMVVM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestRACMVVM
+//
+//  Created by aigegou on 2017/2/28.
+//  Copyright © 2017年 aigegou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
